7633b491fdafb357f9f766c3e5d95924894e6dbb
hilton <hilton.mendes.cardoso@hotmail.com>
2018-10-20 20:17:03 -0300

    Primeiro commit

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => { res.sendFile(path.join(__dirname + 'public/index.html')) });
+
+module.exports = router;
